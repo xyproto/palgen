bee428e55ec065acd55fbfb88e3aca416bfe38dd
Alexander F. Rødseth <rodseth@gmail.com>
2019-07-16 23:20:34 +0200

    Add tests

--- palgen_test.go
+++ palgen_test.go
@@ -19,22 +19,61 @@ func TestGenerate(t *testing.T) {
 		t.Error(err)
 	}
 
-	// Generate a palette with 2 colors
-	_, err = Generate(img, 2)
+	// Generate a palette with 4 colors
+	_, err = Generate(img, 4)
 	if err != nil {
 		t.Error(err)
 	}
 	//fmt.Println("Palette with 2 colors", pal)
+}
+
+func TestSample(t *testing.T) {
+	// Read a truecolor PNG file
+	data, err := os.Open("testdata/sample.png")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Decode the PNG image
+	img, err := png.Decode(data)
+	if err != nil {
+		t.Error(err)
+	}
 
 	// Generate a palette with 256 colors
 	pal, err := Generate(img, 256)
 	if err != nil {
 		t.Error(err)
 	}
-	//fmt.Println("Palette with 256 colors", pal)
 
-	// Output a .gpl palette file in testdata/output.gpl
-	err = Write(pal, "testdata/output.gpl", "Testing123")
+	// Output a .gpl palette file in testdata/output1.gpl
+	err = Write(pal, "testdata/output.gpl", "From sample.png")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSample2(t *testing.T) {
+	// Read a truecolor PNG file
+	data, err := os.Open("testdata/sample2.png")
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Decode the PNG image
+	img, err := png.Decode(data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Generate a palette with 256 colors
+	pal, err := Generate(img, 256)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Output a .gpl palette file in testdata/output2.gpl
+	err = Write(pal, "testdata/output2.gpl", "From sample2.png")
 	if err != nil {
 		t.Error(err)
 	}
