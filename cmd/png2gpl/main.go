fb7dac32fad71d23c1042bb4277d8b2421b2ed49
Alexander F. Rødseth <rodseth@gmail.com>
2019-07-18 17:13:51 +0200

    Add png2gpl utility

---
+++ cmd/png2gpl/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/xyproto/palgen"
+	"image/color"
+	"image/png"
+	"os"
+	"sort"
+)
+
+func main() {
+
+	var (
+		outputFilename string
+		version        bool
+		paletteName    string
+	)
+
+	flag.StringVar(&outputFilename, "o", "-", "output GPL palette")
+	flag.BoolVar(&version, "v", false, "version")
+	flag.StringVar(&paletteName, "n", "Untitled", "palette name")
+
+	flag.Parse()
+
+	if version {
+		fmt.Println("png2gpl 1.0.0")
+		os.Exit(0)
+	}
+
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Fprintf(os.Stderr, "An input PNG filename is required.\n")
+		os.Exit(1)
+	}
+
+	inputFilename := args[0]
+
+	// Open the PNG file
+	f, err := os.Open(inputFilename)
+	m, err := png.Decode(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+	f.Close()
+
+	// Prepare to output the new PNG data to either stdout or to file
+	if outputFilename == "-" {
+		f = os.Stdout
+	} else {
+		f, err = os.Create(outputFilename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
+	// Create a new palette, with 256 colors (the rest of the code does not assume 256)
+	pal, err := palgen.Generate(m, 256)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Remove the alpha
+	for i, c := range pal {
+		rgba := color.RGBAModel.Convert(c).(color.RGBA)
+		rgba.A = 255
+		pal[i] = rgba
+	}
+
+	// Sort the palette
+	spal := palgen.SortablePalette(pal)
+	sort.Sort(spal)
+	pal = color.Palette(spal)
+
+	// Output the GPL palette
+	f.Write([]byte(palgen.GPL(pal, paletteName)))
+	f.Close()
+}
