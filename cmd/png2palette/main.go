41366fa8cc8038cfd504277a9609e9012529664a
Alexander F. Rødseth <rodseth@gmail.com>
2019-07-18 22:37:22 +0200

    Refactor the functionality that can render palettes to images

--- cmd/png2palette/main.go
+++ cmd/png2palette/main.go
@@ -4,8 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"github.com/xyproto/palgen"
-	"image"
-	"image/color"
 	"image/png"
 	"os"
 )
@@ -72,56 +70,10 @@ func main() {
 	// Sort the palette by hue, luminance and chroma
 	palgen.Sort(pal)
 
-	// Remove the alpha
-	for i, c := range pal {
-		rgba := color.RGBAModel.Convert(c).(color.RGBA)
-		rgba.A = 255
-		pal[i] = rgba
-	}
+	// Render the palette as an image
+	palImage := palgen.Render(pal)
 
-	// The first color is now the darkest one
-	darkIndex := uint8(0)
-
-	// Let each row be blocks 32 wide
-	w := 32
-	h := len(pal) / w
-	leftover := len(pal) % w
-	if leftover > 0 {
-		h++
-	}
-
-	// "pixel" width and height
-	pw := 16
-	ph := 16
-
-	upLeft := image.Point{0, 0}
-	lowRight := image.Point{w * pw, h * ph}
-
-	// Create a new image, where a square is painted per color in the palette
-	palImage := image.NewPaletted(image.Rectangle{upLeft, lowRight}, pal)
-
-	// Set color for each pixel.
-	colorIndex := uint8(0)
-OUT:
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			for by := 0; by <= ph; by++ {
-				for bx := 0; bx <= pw; bx++ {
-					if bx == 0 || by == 0 || bx == pw-1 || by == ph-1 {
-						palImage.SetColorIndex((x*pw)+bx, (y*ph)+by, darkIndex)
-					} else {
-						palImage.SetColorIndex((x*pw)+bx, (y*ph)+by, colorIndex)
-					}
-				}
-			}
-			colorIndex++
-			if int(colorIndex) >= len(pal) {
-				break OUT
-			}
-		}
-	}
-
-	// Output the generated image
+	// Output the rendered image
 	if err := png.Encode(f, palImage); err != nil {
 		f.Close()
 		fmt.Fprintf(os.Stderr, "error: %s\n", err)
